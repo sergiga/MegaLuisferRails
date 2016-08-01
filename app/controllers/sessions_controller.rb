ade407f93b79277117ed6a800a1d8485aa6d9316
Sergio <srgarcia91@gmail.com>
2016-08-01 20:39:32 +0200

    Added user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create  # Log in action
+    user =  User.find_by(username: params[:username]) ||
+            User.find_by(email: params[:username])
+    if user && user.authenticate(params[:password])
+      # Log the user in the home page
+    else
+      # Show errors highlighting the fields
+      flash.now[:danger] =  'El nombre de usuario y la contraseña que ingresaste
+                            no coinciden con nuestros registros. Por favor,
+                            revisa e inténtalo de nuevo.'
+      render 'new'
+    end
+  end
+
+  def destroy # Log out action
+  end
 end
