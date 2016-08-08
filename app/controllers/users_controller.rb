3ef991ebfe74a60772c05e89777744f6b4d937e1
Sergio <srgarcia91@gmail.com>
2016-08-08 19:00:44 +0200

    Make 'create user' user case

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit( :name, :username, :email, :password,
+                                    :password_confirmation)
+    end
 end
