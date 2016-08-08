041b2491f60cadc6dc58d2b01849bd28273bd8ac
Sergio <srgarcia91@gmail.com>
2016-08-08 20:35:29 +0200

    Make edit user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users
+      redirect_to users_path
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
