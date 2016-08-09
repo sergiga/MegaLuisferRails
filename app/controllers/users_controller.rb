fd5942f5aebb71d9ea6f5691596d4808a091a13f
Sergio <srgarcia91@gmail.com>
2016-08-09 18:23:13 +0200

    Make edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    debugger
+    if @user.update_attributes(user_params)
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def user_params
       params.require(:user).permit( :name, :username, :email, :password,
