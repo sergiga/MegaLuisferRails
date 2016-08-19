dfe813d900c824b20ff04a20d146e93031b6719f
Borja <borjagarciavillanueva@gmail.com>
2016-08-19 20:52:44 +0200

    Make new component description funcionality

--- app/controllers/component_descs_controller.rb
+++ app/controllers/component_descs_controller.rb
@@ -3,4 +3,35 @@ class ComponentDescsController < ApplicationController
 	def index
 		@component_descs = ComponentDesc.all
 	end
+
+	def new
+		@component_desc = ComponentDesc.new
+	end
+
+	def create
+		@component_desc = ComponentDesc.new(component_params)
+		if @component_desc.save
+			redirect_to component_descs_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@component_desc = ComponentDesc.find(params[:id])
+	end
+
+	def update
+		@component_desc = ComponentDesc.find(params[:id])
+		if @component_desc.update_attributes(component_params)
+			redirect_to component_descs_path
+		else
+			render 'edit'
+		end
+	end
+
+	private
+	def component_params
+		params.require(:component_desc).permit(:componentType, :brand, :model, :features, :alert)
+	end
 end
