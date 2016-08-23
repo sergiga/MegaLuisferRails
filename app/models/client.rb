bf8f5fae51135b0168c152fc8694ac263b6365e6
Sergio <srgarcia91@gmail.com>
2016-08-23 14:00:22 +0200

    Front end validation for new client

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,10 @@ class Client < ApplicationRecord
   has_many :repairs, through: :bills
   accepts_nested_attributes_for :bills
 
+  validates :cif_dni, presence: true
+  validates :name, presence: true
+  validates :contact_phone, presence: true
+
   def self.search (search)
     @client = Client.find_by(cif_dni: search) || Client.new
   end
