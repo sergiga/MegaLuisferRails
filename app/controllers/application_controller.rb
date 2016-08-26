66f42707c835d07ae3af345243288f0f28b53c3e
Sergio <srgarcia91@gmail.com>
2016-08-26 19:24:54 +0200

    Sidebar responsive popup and dropdown warning styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,11 @@ class ApplicationController < ActionController::Base
   before_action :check_stock
 
   def check_stock
-  	@components_descriptions = ComponentDesc.all
-  	@alert_count = 0
-    @low_components = []
-  	
-    @components_descriptions.each do |c|
-  		@component_count = c.alert
-  		@alert_count += 1 if c.components.count <= @component_count
-      @low_components.push(c) if c.components.count <= @component_count
+  	components_descriptions = ComponentDesc.all
+    @components_warnings = []
+
+    components_descriptions.each do |c|
+      @components_warnings.push(c) if c.components.count <= c.alert
   	end
   end
 end
